23b2fb0a99a29b1b1c0ada812728d9f53e1afc56
Matt Bongiorno <bigiorno@gmail.com>
2020-12-09 13:54:11 -0500

    connections.js

---
+++ database/sql/connections.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+
+// Accessing the database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+});
+
+module.exports = connection;
